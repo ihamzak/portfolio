154a16a1bb4018d326a37887bbeb8a3357d93cc0
ihamzak <ham-xa@hotmail.com>
2021-02-07 00:59:02 +0000

    added all the related models

--- portfolio/dashboard/admin.py
+++ portfolio/dashboard/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Language)
+admin.site.register(Database)
+admin.site.register(Library)
+admin.site.register(Datascience)
+admin.site.register(Cloudskill)
+admin.site.register(Certification)
+admin.site.register(Project)
+
+
+
--- portfolio/dashboard/models.py
+++ portfolio/dashboard/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
 # Create your models here.
+class Language(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Database(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Datascience(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
+class Cloudskill(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
+class Certification(models.Model):
+    name = models.CharField(max_length=500)
+    credentials= models.URLField()
+
+    def __str__(self):
+        return self.name
+
+class Project(models.Model):
+    title= models.CharField(max_length=500)
+    description = models.TextField()
+    github = models.URLField()
+
+    def __str__(self):
+        return self.name
+    
+
+
+
+
+
